5df4755e7bdef800320e895ba0644efb5c9d7c90
plesen666 <diada.var@mail.ru>
2024-07-07 20:35:37 +0300

    hw1-

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//1
+        int salary= 15000 ;// сколько
+        int total=0;//кошелек с накоплением
+        int month = 0;//месяца
+                while(  total<2459000) { //копим до 2459000
+                    total=total+total/100;
+                    total=total+salary;
+                    month++;
+                    System.out.println("Месяц " + month + " сумма накоплений равна " + total + " рублей. ");
 
-        }
+//2
+
+
+
+
+
+                    
     }
 }
